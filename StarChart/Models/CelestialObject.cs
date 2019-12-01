dda55f24bf0e3e9f96c571f50e03c021cf321d16
Alyssa Plan <alyssa@alyssaplan.com>
2019-12-01 16:18:22 -0500

    adding "NotMapped" property to Satellites

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StarChart.Models
 {
@@ -10,6 +9,7 @@ namespace StarChart.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
     }
