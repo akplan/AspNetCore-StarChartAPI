0b1f7c9347c6b4414ac18d7a51e9fd43d5587cf1
Alyssa Plan <alyssa.plan@gmail.com>
2019-12-01 16:11:40 -0500

    created CelestialObject model, added CelestialObject model to ApplicationDbContext

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? OrbitedObjectId { get; set; }
+        public List<CelestialObject> Sattelites { get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
